07856d15611301124598566483af706f51272257
IgorLuzhnyi <luzhnyi.igor@gmail.com>
2022-08-02 23:39:36 +0300

    hw#17 refined

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, HashRouter, Route } from "react-router-dom";
 import ContactData from "./components/ContactData";
 import Contracts from "./components/Contracts";
 import HomePage from "./components/HomePage";
@@ -8,13 +8,13 @@ import Posts from "./components/Posts";
 function App() {
   return (
     <div className="App">
-      <Router>
+      <HashRouter>
         <Route path="/" exact component={HomePage} />
         <Route path="/posts" component={Posts} />
         <Route path="/movies" component={Movies} />
         <Route path="/contacts" exact component={Contracts} />
         <Route path="/contacts/:test" component={ContactData} />
-      </Router>
+      </HashRouter>
     </div>
   );
 }
