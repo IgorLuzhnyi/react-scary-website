e037032d80815a361f0c06a8c2977f682317e936
IgorLuzhnyi <luzhnyi.igor@gmail.com>
2022-08-03 10:02:17 +0300

    hw#17 refining hope final

--- src/App.js
+++ src/App.js
@@ -9,13 +9,11 @@ function App() {
   return (
     <div className="App">
       <HashRouter>
-        <Switch>
-          <Route path="/" exact component={HomePage} />
-          <Route path="/posts" component={Posts} />
-          <Route path="/movies" component={Movies} />
-          <Route path="/contacts" exact component={Contracts} />
-          <Route path="/contacts/:test" component={ContactData} />
-        </Switch>
+        <Route path="/" exact component={HomePage} />
+        <Route path="/posts" component={Posts} />
+        <Route path="/movies" component={Movies} />
+        <Route path="/contacts" exact component={Contracts} />
+        <Route path="/contacts/:test" component={ContactData} />
       </HashRouter>
     </div>
   );
